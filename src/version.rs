c6d8cad3dc85224a55919f28d01146cd398446ba
Takeru Ohta <phjgt308@gmail.com>
2017-02-01 21:42:32 +0900

    Add `version` module

---
+++ src/version.rs
@@ -0,0 +1,31 @@
+use std::fmt;
+
+use {Result, Error};
+
+/// HTTP version.
+///
+/// # Examples
+///
+/// ```
+/// use miasht::Version;
+///
+/// assert_eq!(Version::Http1_0.to_string(), "HTTP/1.0");
+/// assert_eq!(Version::Http1_1.to_string(), "HTTP/1.1");
+/// ```
+#[derive(Debug, Clone, Copy, PartialOrd, Ord, PartialEq, Eq, Hash)]
+#[allow(non_camel_case_types)]
+pub enum Version {
+    /// HTTP/1.0.
+    Http1_0,
+
+    /// HTTP/1.1.
+    Http1_1,
+}
+impl fmt::Display for Version {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Version::Http1_0 => write!(f, "HTTP/1.0"),
+            Version::Http1_1 => write!(f, "HTTP/1.1"),
+        }
+    }
+}
